e6c87d5cb75464439873b5d3c8028b218ae759bf
KristineStateler <32514809+KristineStateler@users.noreply.github.com>
2018-05-16 15:11:13 -0400

    debugging in production using puts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    puts "AAAAAAHHHHH Hello from the facebook controller wooooOOOOoooooooOOOOOOO"
+    puts request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts @user
 
     if @user.persisted?
+      puts "we persisted!"
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
+      puts "we are being sent to sign up"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
+    end
+    if @user.nil?
+      "BOOOOOOO something went wrong :( BOOOOOO"
+      failure
     end
   end
 
