d8d462d735254ee61b854829606a8bb499ec1641
KristineStateler <32514809+KristineStateler@users.noreply.github.com>
2018-05-17 11:55:48 -0400

    starting to impliment mobile swipe

--- app/javascript/react/containers/RecipeList.js
+++ app/javascript/react/containers/RecipeList.js
@@ -68,6 +68,51 @@ handleSwipe(type){
   }
 
 
+detectswipe(el,func) {
+  swipe_det = new Object();
+  swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  var min_x = 30;  //min x swipe for horizontal swipe
+  var max_x = 30;  //max x difference for vertical swipe
+  var min_y = 50;  //min y swipe for vertical swipe
+  var max_y = 60;  //max y difference for horizontal swipe
+  var direc = "";
+  ele = document.getElementById(el);
+  ele.addEventListener('touchstart',function(e){
+    var t = e.touches[0];
+    swipe_det.sX = t.screenX;
+    swipe_det.sY = t.screenY;
+  },false);
+  ele.addEventListener('touchmove',function(e){
+    e.preventDefault();
+    var t = e.touches[0];
+    swipe_det.eX = t.screenX;
+    swipe_det.eY = t.screenY;
+  },false);
+  ele.addEventListener('touchend',function(e){
+    //horizontal detection
+    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+      if(swipe_det.eX > swipe_det.sX) direc = "r";
+      else direc = "l";
+    }
+    //vertical detection
+    else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+      if(swipe_det.eY > swipe_det.sY) direc = "d";
+      else direc = "u";
+    }
+
+    if (direc != "") {
+      if(typeof func == 'function') func(el,direc);
+    }
+    direc = "";
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+  },false);
+}
+
+myfunction(el,d) {
+  alert("you swiped on element with id '"+el+"' to "+d+" direction");
+}
+
+
 componentDidMount(){
 fetch('/api/v1/recipes')
   .then(response => {
@@ -170,6 +215,7 @@ if (this.state.recipes[this.state.currentRecipe]) {
             picture={picture}
             steps={steps}
             ingredients={ingredients}
+            detectswipe={this.detectswipe}
            />
            <div className="like-buttons">
               <LikeTile
@@ -182,10 +228,6 @@ if (this.state.recipes[this.state.currentRecipe]) {
             />
           </div>
           </div>
-      </div>
-      <div>
-        <APIContainer
-        />
       </div>
   </div>
     )
